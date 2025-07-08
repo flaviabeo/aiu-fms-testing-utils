eed5ac7a1f091048130eea4557b4187f9764b974
Flavia Beo <flavia.beo@ibm.com>
2025-07-08 16:07:38 -0300

    wip
    Signed-off-by: Flavia Beo <flavia.beo@ibm.com>

--- scripts/generate_layers_metrics.py
+++ scripts/generate_layers_metrics.py
@@ -378,12 +378,20 @@ def generate_layers_metrics(model_path, batch_size, seq_length, max_new_tokens):
                 abs_diff_path = os.path.join(output_dir, f"{prefix}--{layer_name}.abs_diff.csv")
                 cos_sim_path = os.path.join(output_dir, f"{prefix}--{layer_name}.cos_sim.csv")
 
+                cos_sim_res = None
+
+                if isinstance(cos_sim, list):
+                    print(f"{cos_sim}")
+                    cos_sim_res = cos_sim
+                else:
+                    cos_sim_res = cos_sim.flatten().tolist()
+
                 if not os.path.exists(abs_diff_path):
                     logger.debug("saving abs_diff files")
                     write_csv(abs_diff.flatten().tolist(), abs_diff_path, "abs_diff", tensor_cuda_out.shape, tensor_cpu_out.shape, abs_diff.shape)
                 if not os.path.exists(cos_sim_path):
                     logger.debug("saving cos_sim files")
-                    write_csv(cos_sim.flatten().tolist(), cos_sim_path, "cos_sim", tensor_cuda_out.shape, tensor_cpu_out.shape, cos_sim.shape)
+                    write_csv(cos_sim_res, cos_sim_path, "cos_sim", tensor_cuda_out.shape, tensor_cpu_out.shape, cos_sim.shape)
 
     logger.info(f"Completed {model_path} layers' metrics generation")
 
